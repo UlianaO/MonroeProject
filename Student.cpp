88f941107b4d132ad472bc65eddd685c9f786853
Uliana Ozerova <ulyana.ozerova@gmail.com>
2022-09-12 20:33:00 -0400

    mm

--- Student.cpp
+++ Student.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 #include "StudentList.h"
 
+using namespace std;
+
+
 void printStudent(Student s) {
-	std::cout << "Name: " << s.getName() << std::endl;
-	std::cout << "ID: " << s.getId() << std::endl;
-	std::cout << "Email: " << s.getEmail() << std::endl;
-	std::cout << "G1: " << s.getPresentationGrade() << std::endl;
-	std::cout << "G2: " << s.getEssayGrade() << std::endl;
-	std::cout << "G3: " << s.getProjectGrade() << std::endl;
+
+	std::cout << left << setw(20) << s.getName();
+	std::cout << left << setw(20) << s.getId();
+	std::cout << left << setw(20) << s.getEmail();
+	std::cout << left << setw(20) << s.getPresentationGrade();
+	std::cout << left << setw(20) << s.getEssayGrade();
+	std::cout << left << setw(20) << s.getProjectGrade() << endl;
+}
+
+using namespace std;
+
+void showAllRecords(StudentList sl) {
+
+	cout << left << setw(20) << "Name"
+		<< left << setw(20) << "USF ID"
+		<< left << setw(20) << "Email"
+		<< left << setw(20) << "Grade1"
+		<< left << setw(20) << "Grade2"
+		<< left << setw(20) << "Grade3" << endl;
+
+	for (Student s : sl.getStudents())
+	{
+		printStudent(s);
+	}
+
 }
 
 int main() {
@@ -18,11 +41,36 @@ int main() {
 	std::string filePath = "Students.csv";
 
 	StudentList sl = StudentList(filePath);
+	
+	//show all students
+	showAllRecords(sl);
 
-	for (Student s : sl.getStudents())
-	{
-		printStudent(s);
-	}
+	char user_choice;
+
+	// if append is chosen, we are asking to provide the information - that is, we have to create the student - Student(string student info)
+
+	std::fstream studentFile;
+	studentFile.open(filePath, std::ios::app);
+
+	// MAIN MENU HERE
+
+
+
+	std::cout << "Click a to append the student: " << std::endl;
+	std::cin >> user_choice;
+	do {
+		if (user_choice != 'A') {
+			std::cout << "Wrong character, try again.(a)" << std::endl;
+			std::cin >> user_choice;
+		}
+	} while (user_choice != 'a');
+
+		//std::cout << "Wrong input, choose a" << std::endl;
+
+	/*if (studentFile.is_open()) {
+		studentFile << std::endl << "Test File" << ", " << "testing" << ", " << "testmail" << ", " << 3 << ", " << 4 << ", " << 5;
+		studentFile.close();
+	} */
 
 
 	return 0;
